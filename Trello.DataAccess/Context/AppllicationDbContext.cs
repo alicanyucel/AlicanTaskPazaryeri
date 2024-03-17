e6492df9e09036a5ad105a951a0bd89b40bba1a0
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-03-17 22:05:19 +0300

    datacontext

--- Trello.DataAccess/Context/AppllicationDbContext.cs
+++ Trello.DataAccess/Context/AppllicationDbContext.cs
@@ -16,8 +16,11 @@ namespace Trello.DataAccess.Context
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
-      
-        
+
+        public DbSet<Board> Boards { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Column> Columns { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Ignore<IdentityUserLogin<Guid>>();
