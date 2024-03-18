c07c27e8473456beaaf331399e3993dacc029ccf
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-03-18 13:25:11 +0300

    cqrs

--- Trello.Business/obj/Debug/net8.0/Trello.Business.AssemblyInfo.cs
+++ Trello.Business/obj/Debug/net8.0/Trello.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trello.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b8e88607722b7c77de1b67620fb5ee222577ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8408ef36226c62dd665c13d6acac1db1c2cf4e4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trello.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trello.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
